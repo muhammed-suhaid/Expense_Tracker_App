c28cf5ae64bd7e9f74fd2ad60f39af970066496a
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-17 07:12:50 +0530

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ExpensesPage extends StatefulWidget {
+  const ExpensesPage({super.key});
+
+  @override
+  State<ExpensesPage> createState() {
+    return _ExpensesPageState();
+  }
+}
+
+class _ExpensesPageState extends State<ExpensesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Column(
+      children: [],
+    ));
+  }
+}
